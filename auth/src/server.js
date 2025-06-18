848a41e2b2c795275d77ae441d4aab38c2e1e442
sharath mohan <sharathmohan50@gmail.com>
2025-06-18 22:07:16 +0530

    sixth

--- auth/src/server.js
+++ auth/src/server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 // app listening
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on PORT:${PORT}`);
 });
